636c621d28ac9ddb0e3374aa2767b58e9779e3cd
Nitin Sharma <nitinsharma@localhost.localdomain>
2020-02-13 23:00:07 +0530

    first commit

---
+++ ubuntu/bionic64/Kubeadm-install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+export DEBIAN_FRONTEND=noninteractive 
+apt-get update \
+    && apt-get install -y \
+        apt-transport-https \
+        ca-certificates \
+        curl \
+        software-properties-common \
+    && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add - \
+    && add-apt-repository \
+        "deb https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") \
+        $(lsb_release -cs) \
+        stable" \
+    && apt-get update \
+    && apt-get install -y docker-ce=$(apt-cache madison docker-ce | grep 18.06 | head -1 | awk '{print $3}')
+apt-get install -y iptables arptables ebtables apt-transport-https curl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+apt-get update \
+    && apt-get install -y kubelet kubeadm kubectl
+apt-mark hold kubelet kubeadm kubectl
+
